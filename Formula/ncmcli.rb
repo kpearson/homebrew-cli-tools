d333d817d3f5f0342e6cccd7e6ac76b7bae81e37
Kit Pearson <dev_kit@icloud.com>
2020-04-06 23:50:17 -0700

    Bump version 1.1.0

--- Formula/ncmcli.rb
+++ Formula/ncmcli.rb
@@ -1,9 +1,9 @@
 class Ncmcli < Formula
   desc "A command line tool for NCM type things"
   homepage "https://github.com/kpearson/ncmcli"
-  url "https://github.com/kpearson/cli-tools/raw/master/archive/ncmcli/ncmcli-1.0.0.tar.gz"
-  sha256 "1199518c0d90fc63f01de228cd8fed602771d6a3d24458ed2a4cace81a38170a"
-  version "1.0.0"
+  url "https://github.com/kpearson/cli-tools/raw/master/archive/ncmcli/ncmcli-1.1.0.tar.gz"
+  sha256 "5c820a78e4409b54d3fc35cd3de1fd915377c3992c506e73db281260f12e8baf"
+  version "1.1.0"
 
   bottle :unneeded
 
